a38808fead51337f1bd2271e800b6def9f6c57e3
Sébastien Weber <seba.weber@gmail.com>
2026-01-21 09:51:48 +0100

    Create daq_0Dviewer_Photodiode.py

---
+++ src/pymodaq_plugins_teaching/daq_viewer_plugins/plugins_0D/daq_0Dviewer_Photodiode.py
@@ -0,0 +1,136 @@
+import numpy as np
+from llvmlite.binding import initialize
+
+from pymodaq_utils.utils import ThreadCommand
+from pymodaq_data.data import DataToExport
+from pymodaq_gui.parameter import Parameter
+
+from pymodaq.control_modules.viewer_utility_classes import DAQ_Viewer_base, comon_parameters, main
+from pymodaq.utils.data import DataFromPlugins
+
+from pymodaq_plugins_teaching.hardware.spectrometer import Spectrometer
+
+
+class DAQ_0DViewer_Photodiode(DAQ_Viewer_base):
+    """ Instrument plugin class for a OD viewer.
+    
+    This object inherits all functionalities to communicate with PyMoDAQ’s DAQ_Viewer module through inheritance via
+    DAQ_Viewer_base. It makes a bridge between the DAQ_Viewer module and the Python wrapper of a particular instrument.
+
+    TODO Complete the docstring of your plugin with:
+        * The set of instruments that should be compatible with this instrument plugin.
+        * With which instrument it has actually been tested.
+        * The version of PyMoDAQ during the test.
+        * The version of the operating system.
+        * Installation instructions: what manufacturer’s drivers should be installed to make it run?
+
+    Attributes:
+    -----------
+    controller: object
+        The particular object that allow the communication with the hardware, in general a python wrapper around the
+         hardware library.
+         
+    # TODO add your particular attributes here if any
+
+    """
+    params = comon_parameters+[
+        ## TODO for your custom plugin: elements to be added here as dicts in order to control your custom stage
+        ]
+
+    def ini_attributes(self):
+        #  TODO declare the type of the wrapper (and assign it to self.controller) you're going to use for easy
+        #  autocompletion
+        self.controller: Spectrometer = None
+
+        #TODO declare here attributes you want/need to init with a default value
+        pass
+
+    def commit_settings(self, param: Parameter):
+        """Apply the consequences of a change of value in the detector settings
+
+        Parameters
+        ----------
+        param: Parameter
+            A given parameter (within detector_settings) whose value has been changed by the user
+        """
+        ## TODO for your custom plugin
+        if param.name() == "a_parameter_you've_added_in_self.params":
+           self.controller.your_method_to_apply_this_param_change()  # when writing your own plugin replace this line
+#        elif ...
+        ##
+
+    def ini_detector(self, controller=None):
+        """Detector communication initialization
+
+        Parameters
+        ----------
+        controller: (object)
+            custom object of a PyMoDAQ plugin (Slave case). None if only one actuator/detector by controller
+            (Master case)
+
+        Returns
+        -------
+        info: str
+        initialized: bool
+            False if initialization failed otherwise True
+        """
+
+        if self.is_master:
+            self.controller = Spectrometer()  #instantiate you driver with whatever arguments are needed
+            initialized = self.controller.open_communication() # call eventual methods
+        else:
+            self.controller = controller
+            initialized = True
+
+        info = "Whatever info you want to log"
+        return info, initialized
+
+    def close(self):
+        """Terminate the communication protocol"""
+        ## TODO for your custom plugin
+        raise NotImplementedError  # when writing your own plugin remove this line
+        if self.is_master:
+            #  self.controller.your_method_to_terminate_the_communication()  # when writing your own plugin replace this line
+            ...
+
+    def grab_data(self, Naverage=1, **kwargs):
+        """Start a grab from the detector
+
+        Parameters
+        ----------
+        Naverage: int
+            Number of hardware averaging (if hardware averaging is possible, self.hardware_averaging should be set to
+            True in class preamble and you should code this implementation)
+        kwargs: dict
+            others optionals arguments
+        """
+        ## TODO for your custom plugin: you should choose EITHER the synchrone or the asynchrone version following
+
+        data_tot = self.controller.grab_monochromator()
+        self.dte_signal.emit(
+            DataToExport(name='Photodiode',
+                         data=[
+                             DataFromPlugins(name='Photodiode', data=[data_tot],
+                                             labels=['Intensity'],
+                                             units='V')]))
+
+
+    def callback(self):
+        """optional asynchrone method called when the detector has finished its acquisition of data"""
+        data_tot = self.controller.your_method_to_get_data_from_buffer()
+        self.dte_signal.emit(DataToExport(name='myplugin',
+                                          data=[DataFromPlugins(name='Mock1', data=data_tot,
+                                                                dim='Data0D', labels=['dat0', 'data1'])]))
+
+    def stop(self):
+        """Stop the current grab hardware wise if necessary"""
+        ## TODO for your custom plugin
+        raise NotImplementedError  # when writing your own plugin remove this line
+        self.controller.your_method_to_stop_acquisition()  # when writing your own plugin replace this line
+        self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
+        ##############################
+        return ''
+
+
+if __name__ == '__main__':
+    main(__file__)
